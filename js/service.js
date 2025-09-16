e579c6def6ae311c98407ee4c6c873a76fec5561
naymur-code <naymur.dev25@gmail.com>
2025-09-17 01:00:33 +0600

    call feature applay

--- js/service.js
+++ js/service.js
@@ -8,3 +8,53 @@ for (const heart of heartBtn) {
     document.getElementById("heartText").innerText = count;
   });
 }
+
+// coll feature 
+const collBtn = document.getElementsByClassName("coll-btn");
+let coin = parseInt(document.getElementById("coin").innerText);
+for (const coll of collBtn) {
+  coll.addEventListener("click", function (event) {
+    const item = event.target.parentNode.parentNode;
+    const callHistory = document.getElementById("call-history");
+
+    if (coin >= 20) {
+      alert(
+        `
+      ${item.children[1]?.innerText}
+      ${item.children[3]?.innerText}
+      `
+      );
+
+      const newElement = document.createElement("div");
+      newElement.classList.add(
+        "flex",
+        "justify-between",
+        "p-4",
+        "bg-white",
+        "rounded-lg"
+      );
+      newElement.innerHTML = `
+                    <div>
+                  <h3 class="font-bold text-lg">${
+                    item.children[1]?.innerText
+                  }</h3>
+                        <p>${item.children[3]?.innerText}</p>
+                    </div>              
+                       <p>${new Date().toLocaleTimeString()}</p>
+      `;
+      callHistory.appendChild(newElement);
+
+      coin = coin - 20;
+    } else {
+      alert("Not coin!");
+      return;
+    }
+    document.getElementById("coin").innerText = coin;
+  });
+}
+
+// clear history
+document.getElementById("clear-btn").addEventListener("click", function () {
+  const callHistory = document.getElementById("call-history");
+  callHistory.innerHTML = "";
+});
